a6e05dc0c9430d28a5d5b8c1b4e9db2137905721
Stephen <smjarr89@gmail.com>
2018-04-30 16:45:33 -0400

    added functionality to fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,24 @@
-c
+
+def fizzbuzz():
+    for num in range(1,101):
+        if num % 3 == 0 and num % 5 == 0:
+            print(fizzbuzzprint())
+        elif num % 3 == 0:
+            print(fizzprint())
+        elif num % 5 == 0:
+            print(buzzprint())
+        else: 
+            print(num)
+    return
+
+def fizzprint():
+    message = 'Fizz'
+    return message
+def buzzprint():
+    message = 'Buzz'
+    return message
+def fizzbuzzprint():
+    message = 'FizzBuzz'
+    return message
+
+fizzbuzz()
